dabc1b47d2855c5371c1fa623e34e84dbc5057b4
danghaag <danghaag@gmail.com>
2014-02-07 13:44:17 -0600

    Initial Commit

---
+++ XCode-test1/XCode-test1/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  XCode-test1
+//
+//  Created by Daniel Haag on 2/7/14.
+//  Copyright (c) 2014 Daniel Haag. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
