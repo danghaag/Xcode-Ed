403c9de748c65403e5c5556d8646cc671c2a2fe1
danghaag <danghaag@gmail.com>
2014-02-07 13:53:57 -0600

    Foundation1

---
+++ Foundation-test1/Foundation-test1/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Foundation-test1
+//
+//  Created by Daniel Haag on 2/7/14.
+//  Copyright (c) 2014 Daniel Haag. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
