9ec7c2be56630cbaeca74064a02b9a117d6c8322
danghaag <danghaag@gmail.com>
2014-02-07 13:52:10 -0600

    test2 -1

---
+++ Test2/Test2/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Test2
+//
+//  Created by Daniel Haag on 2/7/14.
+//  Copyright (c) 2014 Daniel Haag. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
